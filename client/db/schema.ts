ddf111465cbf5232a2b1c2ea696fbaaeda6faeb8
ismaeelbashir03 <ismaeelbashir2003@gmail.com>
2024-11-03 05:47:46 +0000

    added schema

---
+++ client/db/schema.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const Level = {
+    CRITICAL: 'critical',
+    ERROR: 'error',
+    WARN: 'warn',
+    INFO: 'info',
+    DEBUG: 'debug'
+};
+
+const AlertSchema = new Schema({
+  appName: String,
+  severity: Level,
+  cron: Date,
+  messageContains: String,
+});
+
+export default mongoose.model('Alert', AlertSchema);
